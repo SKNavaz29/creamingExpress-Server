52e7bf81904da70b5c10d6c01ef80a15962192c8
Syed Khaja Navaz Ali <sknavaz29@gmail.com>
2023-06-03 16:09:59 +0530

    first commit

--- app.js
+++ app.js
@@ -44,11 +44,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.use('/items', itemRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
